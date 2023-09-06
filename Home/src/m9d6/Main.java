bf793d499484eb157c1ef88b121462ed95c8cf2e
HwangDooHyeon <geg5534@gmail.com>
2023-09-07 01:02:18 +0900

    asdf

--- Home/src/m9d6/Main.java
+++ Home/src/m9d6/Main.java
@@ -1,30 +1,37 @@
+package m9d6;
+
 public class Main {
     public static void main(String[] args) {
 
+
+
+        /*
         int value = 30;
         int result = value + 10;
 
         System.out.println(value);
+        */
 
 
 
+        /*
         int hour = 3;
         int minute = 5;
 
         System.out.println(hour + "시간 " + minute + "분");
 
-
-
         int totalTime = (hour*60) + minute;
 
         System.out.println("총 " + totalTime + "분");
+        */
 
 
 
+        /*
         int x = 3;
         int y = 5;
         int temp = 0;
-        
+
         System.out.println("| X : " + x + " | Y : " + y + " | temp : " + temp + " |");
 
         temp = x;
@@ -38,7 +45,12 @@ public class Main {
         y = temp;
 
         System.out.println("| X : " + x + " | Y : " + y + " | temp : " + temp + " |");
+        */
 
+
+
+        /*
+        */
 
 
 
